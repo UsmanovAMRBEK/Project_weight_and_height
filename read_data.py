af824a51277ef1c21114b2c6f9065758beb8ac76
UsmanovAMRBEK <princariens@gmail.com>
2022-05-19 06:21:32 +0500

    read

--- read_data.py
+++ read_data.py
@@ -1,4 +1,5 @@
 import csv
+import pathlib as pl
 
 def read_csv_data(file_path):
     """
@@ -10,5 +11,12 @@ def read_csv_data(file_path):
     """
     result_data = []
     # WRITE YOUR CODE HERE
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            result_data.append(row)
     return result_data
+
+path = pl.Path('data')
+file_path = path / "weight-height.csv"
     
